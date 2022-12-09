77c1a25593e86fd10a6e3b7209ca281214b477bd
Alphamj <mjack306@gmail.com>
2022-12-09 10:35:20 -0800

    Task 2.0

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char val;
+
+	for(val = 'a'; val <= 'z'; val++)
+		putchar(val);
+
+	putchar('\n');
+
+	return (0);
+}
