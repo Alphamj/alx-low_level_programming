4fbaa728bc54733aea9707a53afa635d626d82b7
Alphamj <mjack306@gmail.com>
2022-12-09 11:43:03 -0800

    Task 5.0

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - Task 5
+ *
+ * Return: Always 0
+ */
+int main(void)
+{
+	int val;
+
+	for (val = 0; val < 10; val++)
+		printf("%d", val);
+
+	printf("\n");
+
+	return (0);
+}
