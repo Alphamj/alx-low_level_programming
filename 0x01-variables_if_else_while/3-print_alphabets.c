2cc5046f14eef42e5b04d887740a3bfd80dbfec8
Alphamj <mjack306@gmail.com>
2022-12-09 11:02:51 -0800

    Task 3.1

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - alphABET
+ *
+ * Return: Always 0 
+ */
+int main(void)
+{
+	char val;
+
+	for (val = 'a'; val <= 'z'; val++)
+		putchar(val)
+	for (val = 'A'; val <= 'Z'; val++)
+		putchar(val);
+	putchar('\n');
+
+	return (0);
+}
