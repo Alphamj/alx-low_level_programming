c7cd6f9de032ec567b908e2d32a2073b020e295d
Alphamj <mjack306@gmail.com>
2022-12-13 11:59:28 -0800

    Task 2.0

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,21 @@
+nclude "main.h"
+
+/**
+ * print_alphabet_10x - Prints 10x the alphabet
+ *
+ * Return: 10x a-z
+ */void print_alphabet(void)
+{
+	int n, co;
+
+	co  = 0;
+
+	while (co < 10)
+	{
+		for  (n = 'a'; n <= 'z'; n++)
+			_putchar(n);
+
+		co++;
+		_putchar('\n');
+	}
+}
