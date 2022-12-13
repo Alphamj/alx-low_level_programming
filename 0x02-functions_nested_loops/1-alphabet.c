df10e623f2d3ea0ab31381c0385f95eb96d8bc6d
Alphamj <mjack306@gmail.com>
2022-12-13 11:46:27 -0800

    Task 1.0

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * main - Entry Point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int n;
+
+	for (n = 'a'; v <= 'z'; n++)
+	{
+		_putchar(n);
+	}
+	_putchar('\n');
+
+	return (0);
+}
